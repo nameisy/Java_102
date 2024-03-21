6dbf08b65def4680935e7fd19508cba14ec516af
İsmail Safa Yılmaz <nameisy@users.noreply.github.com>
2024-03-22 00:41:42 +0300

    Try Catch Pratik

---
+++ src/TryCatchPratik.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class TryCatchPratik {
+    public static void main(String[] args) {
+
+        int[] dizi = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int indeks;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Dizideki elemanın indeksini girin: ");
+        indeks = scanner.nextInt();
+
+        try {
+            System.out.println(dizi[indeks]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Hata! Dizin dışı bir indeks girildi. " + e.getMessage());
+        }
+
+    }
+
+}
