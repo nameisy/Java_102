70565f3efa034e4add0a0883a4a741c631a4e933
İsmail Safa Yılmaz <nameisy@users.noreply.github.com>
2024-03-22 00:48:29 +0300

    Try Catch Pratik

--- src/TryCatchPratik.java
+++ src/TryCatchPratik.java
@@ -6,10 +6,12 @@ class TryCatchPratik {
         int[] dizi = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         int indeks;
 
+        // Kullanıcıdan veri alma ve dizi indeksini isteme
         Scanner scanner = new Scanner(System.in);
         System.out.print("Dizideki elemanın indeksini girin: ");
         indeks = scanner.nextInt();
 
+        // Try-catch bloğu ile hata kontrolü.
         try {
             System.out.println(dizi[indeks]);
         } catch (ArrayIndexOutOfBoundsException e) {
